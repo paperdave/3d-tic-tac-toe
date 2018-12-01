bbe592ac2dd21576ce4ea696ec4e2318432f4e89
Dave <imdaveead@gmail.com>
2018-12-01 16:02:00 -0500

    Server Starting

--- server/index.js
+++ server/index.js
@@ -4,8 +4,11 @@ const chalk = require('chalk');
 
 // Write a default .ENV if one does not exist
 if (!fs.existsSync(".env")) {
-    let dotenvdata = `# Application Configuration File
-`;
+    let dotenvdata = "# Application Configuration File\n"
+                    + "HTTP_PORT=80"
+                    + "HTTPS_PORT=443"
+                    + "HTTPS_KEY=keys/private.key"
+                    + "HTTPS_CERT=keys/certificate.crt"
 
     fs.writeFileSync(".env", dotenvdata);
     console.log(chalk.yellow("A default .env file was created, use this to configure the application."))
@@ -13,3 +16,28 @@ if (!fs.existsSync(".env")) {
 
 // Load .ENV file
 require('dotenv').config();
+
+// Start the express server
+const express = require('express');
+const app = express();
+
+app.use(express.static('../game/'));
+
+// HTTP
+if (process.env.HTTP_PORT) {
+    const server = require('http').Server(app);
+    server.listen(parseInt(process.env.HTTP_PORT), function () {
+        console.log('HTTP Listening on *:' + process.env.HTTP_PORT);
+    });
+}
+
+// HTTPS
+if (process.env.HTTPS_PORT) {
+    var server = require('https').createServer({
+        key: fs.readFileSync(process.env.HTTPS_KEY),
+        cert: fs.readFileSync(process.env.HTTPS_CERT),
+    }, app);
+    server.listen(parseInt(process.env.HTTPS_PORT), function () {
+        console.log('HTTPS Listening on *:' + process.env.HTTPS_PORT);
+    });
+}
